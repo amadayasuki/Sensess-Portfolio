8817adb3c4e184bbd0077a5ddf747af4d1498b67
Sensess <SensessYentzer@outlook.com>
2022-07-21 18:22:47 -0400

    Added Clickable Images to Footer

--- sensess-portfolio/src/components/Footer/Footer.js
+++ sensess-portfolio/src/components/Footer/Footer.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import { Navbar, Container} from 'react-bootstrap';
+import LinkedIn from '../../assets/Linkedin.svg';
 import Github from '../../assets/Github.svg';
+import Instagram from '../../assets/Instagram.svg';
+
 function Footer() {
     return (
-        <Navbar fixed="bottom" bg="dark" id='navbar'>
+        <Navbar fixed="bottom" variant="flat" id='navbar'>
         <Container>
           <Navbar.Brand href="#home">
-             
-            <img
-              src={Github}
-              width="50"
-              height="50"
-              className="d-inline-block align-top"
-              alt="github"
-            /> 
+            <a href="https://github.com">
+          <img
+            src={Github}
+            alt="github"
+          />
+          </a>
+          <a href="https://github.com">
+          <img
+            src={LinkedIn}
+            alt="github"
+          />
+          </a>
+          <a href="https://github.com">
+          <img
+            src={Instagram}
+            alt="github"
+          />
+          </a>
            
           </Navbar.Brand>
         </Container>
--- sensess-portfolio/src/components/Header/Header.js
+++ sensess-portfolio/src/components/Header/Header.js
@@ -5,18 +5,18 @@ import About from '../About/About';
 
 function Header(props) {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" className=" justify-content-between">
           <Container>
             <Navbar.Brand href="#home">Sensess' Portfolio</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+            
+            
               <Nav className="me-auto">
                 <Nav.Link href="#about" onClick = {() => {props.setPage("about")}}>About</Nav.Link>
                 <Nav.Link href="#projects" onClick = {() => {props.setPage("projects")}}>Portfolio</Nav.Link>
                 
                   
               </Nav>
-            </Navbar.Collapse>
+            
           </Container>
         </Navbar>
       );
