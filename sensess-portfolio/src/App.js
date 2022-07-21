9e03de2669120019e6725baf3bbc0dad758275f2
Sensess <SensessYentzer@outlook.com>
2022-07-21 14:38:35 -0400

    File Structure

---
+++ sensess-portfolio/src/App.js
@@ -0,0 +1,18 @@
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import About from './components/About/About';
+import Projects from './components/Projects/Projects';
+import './App.css';
+
+function App() {
+  
+    <div className="App">
+    <Header />
+    <About />
+    <Projects />
+    <Footer />
+    </div>
+
+}
+
+export default App;
---
+++ sensess-portfolio/src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About(props) {
+    return (
+        <div>
+            <h1> This is a About </h1>
+        </div>
+    );
+}
+
+export default About;
